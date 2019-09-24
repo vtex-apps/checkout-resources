dbcfbab417d442dccc80d11b30df80e79bbbb8f7
nandoacoelho <nandoacoelho@gmail.com>
2019-09-24 14:51:19 -0300

    Add select delivery option mutation

--- react/Mutations.js
+++ react/Mutations.js
@@ -1,9 +1,11 @@
 import estimateShipping from './mutations/estimateShipping.graphql'
 import insertCoupon from './mutations/insertCoupon.graphql'
 import updateItems from './mutations/updateItems.graphql'
+import selectDeliveryOption from './mutations/selectDeliveryOption.graphql'
 
 export default {
   insertCoupon,
   estimateShipping,
+  selectDeliveryOption,
   updateItems,
 }
