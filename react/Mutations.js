7c861d191445c2bce9f8052b9d203522c779a8ad
Marcos Kawakami <marcos.kawakami@vtex.com.br>
2019-10-10 18:10:54 -0300

    Add addToCart mutation

--- react/Mutations.js
+++ react/Mutations.js
@@ -1,11 +1,13 @@
+import addToCart from './mutations/addToCart.graphql'
 import estimateShipping from './mutations/estimateShipping.graphql'
 import insertCoupon from './mutations/insertCoupon.graphql'
-import updateItems from './mutations/updateItems.graphql'
 import selectDeliveryOption from './mutations/selectDeliveryOption.graphql'
+import updateItems from './mutations/updateItems.graphql'
 
 export default {
-  insertCoupon,
+  addToCart,
   estimateShipping,
+  insertCoupon,
   selectDeliveryOption,
   updateItems,
 }
