e7d0639533b8a781b30464a0eddda216abd1fe3a
Pedro Soares <pedro.soares@vtex.com.br>
2020-01-24 14:14:48 -0300

    savePaymentToken mutation

--- react/Mutations.tsx
+++ react/Mutations.tsx
@@ -3,6 +3,7 @@ import estimateShipping from './mutations/estimateShipping.graphql'
 import insertCoupon from './mutations/insertCoupon.graphql'
 import selectDeliveryOption from './mutations/selectDeliveryOption.graphql'
 import updateItems from './mutations/updateItems.graphql'
+import savePaymentToken from './mutations/savePaymentToken.graphql'
 
 export default {
   addToCart,
@@ -10,4 +11,5 @@ export default {
   insertCoupon,
   selectDeliveryOption,
   updateItems,
+  savePaymentToken,
 }
